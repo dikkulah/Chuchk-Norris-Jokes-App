c20f107bac238c833d1f5a7a7896141a4b8fa2ce
dikkulah <dikkulah@gmail.com>
2022-02-06 22:14:39 +0300

    Spring projesi oluşturuldu

---
+++ src/main/java/com/dikkulah/cemyilmazjokesapp/CemYilmazJokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.dikkulah.cemyilmazjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CemYilmazJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CemYilmazJokesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dikkulah/cemyilmazjokesapp/CemYilmazJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dikkulah.cemyilmazjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CemYilmazJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
